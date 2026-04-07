852e00b408e068d809f065b7554d7f0770ffcf52
Sergei Uskov <spec256@ya.ru>
2026-04-07 15:01:20 +0500

    add internal/filelock/filelock.go

---
+++ internal/filelock/filelock.go
@@ -0,0 +1,22 @@
+package filelock
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+)
+
+func WithLock(lockPath string, fn func() error) error {
+	f, err := os.OpenFile(lockPath, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return fmt.Errorf("open lock file: %w", err)
+	}
+	defer f.Close()
+
+	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX); err != nil {
+		return fmt.Errorf("acquire lock: %w", err)
+	}
+	defer syscall.Flock(int(f.Fd()), syscall.LOCK_UN)
+
+	return fn()
+}
